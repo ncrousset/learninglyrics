2fecfe74f60cd8c0a43d43cf8da995966bd2fcec
Rudys Natanael Acosta crousset <natanael926@gmail.com>
2016-12-06 20:41:20 -0400

    CRear app lyrics y model lyric

---
+++ learninglyrics/lyrics/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.utils import timezone
+
+class Lyric(models.Model):
+
+    MAYBELANGUAGE = (
+        ('es', 'espaniol'),
+        ('en', 'ingles'),
+    )
+
+    title = models.CharField(max_length=100, verbose_name='Title', db_index=True)
+    context = models.TextField()
+    lannguage = models.CharField(max_length=2, choices=MAYBELANGUAGE, default='en')
+    url_video = models.URLField(max_length=200, null=True)
+    published = models.DateTimeField(auto_now=False, auto_now_add=False)
+    created_date = models.DateTimeField(default=timezone.now(), blank=True)
+    updated_date = models.DateTimeField(null=True, blank=True)
+    created_by = models.ForeignKey('auth.user', on_delete=models.SET_NULL, blank=True, null=True)
