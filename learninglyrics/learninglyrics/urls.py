11eefe3f6d9d5048de23132b64d8a043e37bcfec
Rudys Natanael Acosta crousset <natanael926@gmail.com>
2016-12-12 13:47:39 -0400

    Login y logout con com mi templante

--- learninglyrics/learninglyrics/urls.py
+++ learninglyrics/learninglyrics/urls.py
@@ -1,13 +1,12 @@
 from django.conf.urls import url, include
 from rest_framework import routers
-from account import views
 from django.contrib import admin
 
 router = routers.DefaultRouter()
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^api/', include('account.urls')),
+    url(r'^', include('account.urls')),
     url(r'^api/', include('lyrics.urls')),
     url(r'^api/', include('quiz.urls')),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
