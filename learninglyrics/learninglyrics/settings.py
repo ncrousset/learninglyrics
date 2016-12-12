f0ed016645f0eb063fee53fca6b0088f9cdf6c27
Rudys Natanael Acosta crousset <natanael926@gmail.com>
2016-12-12 13:48:37 -0400

    agregando page por defecto al hacer login y tmeplante por defecto

--- learninglyrics/learninglyrics/settings.py
+++ learninglyrics/learninglyrics/settings.py
@@ -60,11 +60,14 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'learninglyrics.urls'
+LOGIN_REDIRECT_URL = '/api/'
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -131,3 +134,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+
